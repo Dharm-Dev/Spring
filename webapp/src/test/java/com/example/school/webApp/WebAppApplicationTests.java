ae5fd73c03ef29258d73b9506d57b801401e37db
Dharm Vashisth <dharm.vashisth@hexaviewtech.com>
2022-09-15 14:34:31 +0530

    added auto dependencies of spring web app.

---
+++ webapp/src/test/java/com/example/school/webApp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.school.webApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
