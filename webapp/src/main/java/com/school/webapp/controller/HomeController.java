33dab4cedbe57e25e693636c6bb870c678ddd4ac
Dharm Vashisth <dharm.vashisth@hexaviewtech.com>
2022-09-17 18:42:25 +0530

    Dynamic web page using Thymeleaf and dev-tool of spring.

--- webapp/src/main/java/com/school/webapp/controller/HomeController.java
+++ webapp/src/main/java/com/school/webapp/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.school.webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping(value = {"/home","","/study"})
     public String displayhome(){
         return "index.html";
+    }
+
+    @RequestMapping("/dhome")
+    public String displayhomeDynamic(Model model)
+    {
+        model.addAttribute("username","Dharm Vashisth");
+        return "home.html";
     }
 }
